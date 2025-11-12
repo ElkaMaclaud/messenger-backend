61782aac60331d9414f6e02ca449cb300bf95169
ElkaMaclaud <elkamaclaud@gmail.com>
2025-11-12 18:53:24 +0300

    fix: resolve duplicate imports and module configuration errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ChatsModule } from './modules/chats/chats.module';
-import { ChatEntityTsModule } from './modules/chat.entity.ts/chat.entity.ts.module';
-import { ChatsService } from './modules/chats/chats.service';
-import { ChatsService } from './modules/chats/chats/chats.service';
-import { ChatsController } from './modules/chats/chats/chats.controller';
-import { ChatsModule } from './modules/chats/chats.module';
 
 @Module({
   imports: [
@@ -24,10 +19,7 @@ import { ChatsModule } from './modules/chats/chats.module';
     UsersModule,
     AuthModule,
     ChatsModule,
-    ChatEntityTsModule,
   ],
-  providers: [ChatsService],
-  controllers: [ChatsController],
 })
 export class AppModule {}
 //В продакшене synchronize ставим false и используем миграции
